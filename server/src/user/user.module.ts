3aa6013cbe44f0ac788f69e58061f6f29bc715c0
alexKwonIsAwesome <khwalex@gmail.com>
2019-10-30 16:13:47 +0900

    [server] Add user service logic of finding a user by email

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [UserService],
   controllers: [UserController],
   providers: [UserService],
 })

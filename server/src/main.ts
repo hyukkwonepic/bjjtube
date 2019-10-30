475a7bf18571af37e8322d0235b91d6c54e89ea3
alexKwonIsAwesome <khwalex@gmail.com>
2019-10-30 17:35:20 +0900

    [server] Modify main.ts to apply session and passport middlewares

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import * as session from 'express-session';
+import * as passport from 'passport';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(
+    session({
+      secret: 'session-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
